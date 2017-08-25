b58634083d7d11de23bc17bdee7ed9bd877970de
S <442089@cognizant.com>
2017-08-25 14:24:54 +0530

    added user component

---
+++ components/um/adduser.js
@@ -0,0 +1,16 @@
+function adduser(userModel)
+{
+    // 
+    //add user data to repository
+    return new userInfo("rajesh","s");
+}
+
+class userInfo
+{
+    constructor()
+    {
+        this.name = "";
+        this.surname = "";
+    }
+
+}
